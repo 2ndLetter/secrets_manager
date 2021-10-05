10591c604de077760e5ecbd1c357cb8127ff1396
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-10-05 19:51:44 -0400

    Added info

--- get_secrets.sh
+++ get_secrets.sh
@@ -11,3 +11,17 @@ echo "this is the \$USERNAME variable = $USERNAME"
 echo "this is the \$PASSWORD variable = $PASSWORD"
 
 echo -e "this is what the raw output looks like:\n $RAW"
+
+#this is what the raw output looks like:
+# {
+#    "ARN": "arn:aws:secretsmanager:us-east-1:599702904325:secret:ProdAppPassword-4fC1vQ",
+#    "Name": "ProdAppPassword",
+#    "VersionId": "9190d130-1ac0-48a4-8185-05f46cbeefc6",
+#    "SecretString": "{\"username\":\"cloud_user\",\"password\":\"+thiS-is-My_secret-pw\"}",
+#    "VersionStages": [
+#        "AWSCURRENT"
+#    ],
+#    "CreatedDate": "2021-10-05T19:38:07.014000-04:00"
+#}
+#
+# See Secrets_Manager.PNG for AWS Console Screenshot
